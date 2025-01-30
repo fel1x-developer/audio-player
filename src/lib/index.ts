98dc023c0de99d124f36d7bcab6477d53fd601db
Minsoo Choo <minsoochoo0122@proton.me>
2025-01-29 23:14:04 -0500

    Fix index.ts

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,5 @@
 // Reexport your entry components here
 import './components/AudioPlayer/index.css';
-import {default as AudioPlayer} from '$lib/components/AudioPlayer/Player.svelte';
+import AudioPlayer from '$lib/components/AudioPlayer/Player.svelte';
+
+export default AudioPlayer;
