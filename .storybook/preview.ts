18c8c892447fd9a9e64da72e4b32001807572e1c
Minsoo Choo <minsoochoo0122@proton.me>
2025-01-29 22:31:40 -0500

    Update .storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,14 +1,35 @@
-import type { Preview } from '@storybook/svelte'
+import type { Preview } from '@storybook/svelte';
+import { withThemeByClassName, withThemeByDataAttribute } from '@storybook/addon-themes';
+
+import '../src/app.css';
 
 const preview: Preview = {
-  parameters: {
-    controls: {
-      matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
-      },
-    },
-  },
+	parameters: {
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i
+			}
+		}
+	}
 };
+
+export const decorators = [
+	withThemeByDataAttribute({
+		themes: {
+			light: 'light',
+			dark: 'dark'
+		},
+		defaultTheme: 'light',
+		attributeName: 'data-mode'
+	}),
+	withThemeByClassName({
+		themes: {
+			light: 'light',
+			dark: 'dark'
+		},
+		defaultTheme: 'light'
+	})
+];
 
 export default preview;
