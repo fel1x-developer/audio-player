a517772824294921c8f91113bed5cf399c9a456d
Minsoo Choo <minsoochoo0122@proton.me>
2025-02-26 23:09:42 -0500

    Introduce vitest browser mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import tailwindcss from '@tailwindcss/vite';
-import { svelteTesting } from '@testing-library/svelte/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
@@ -7,30 +6,14 @@ export default defineConfig({
 	plugins: [sveltekit(), tailwindcss()],
 
 	test: {
-		workspace: [
-			{
-				extends: './vite.config.ts',
-				plugins: [svelteTesting()],
-
-				test: {
-					name: 'client',
-					environment: 'jsdom',
-					clearMocks: true,
-					include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
-					exclude: ['src/lib/server/**'],
-					setupFiles: ['./vitest-setup-client.ts']
-				}
-			},
-			{
-				extends: './vite.config.ts',
-
-				test: {
-					name: 'server',
-					environment: 'node',
-					include: ['src/**/*.{test,spec}.{js,ts}'],
-					exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
-				}
-			}
-		]
+		name: 'client',
+		clearMocks: true,
+		include: ['src/**/*.svelte.{test,spec}.{js,ts}', 'src/**/*.{test,spec}.{js,ts}'],
+		browser: {
+			enabled: true,
+			provider: 'playwright',
+			// https://vitest.dev/guide/browser/playwright
+			instances: [{ browser: 'chromium' }, { browser: 'firefox' }, { browser: 'webkit' }]
+		}
 	}
 });
